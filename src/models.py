0e657ae5e3695062ef0c8468d6f57cdab63105f0
NicolleMendoza30 <83618165+NicolleMendoza30@users.noreply.github.com>
2025-09-20 00:00:06 +0000

    Modelo de Datos de Instagram

--- src/models.py
+++ src/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean , ForeignKey , DateTime
+from sqlalchemy.orm import Mapped, mapped_column ,  relationship
+import datetime
 
 db = SQLAlchemy()
 
@@ -17,3 +18,43 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Post(db.Model):
+    __tablename__ = "post"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    image_url: Mapped[str] = mapped_column(String(250))
+    caption: Mapped[str] = mapped_column(String(500))
+    timestamp: Mapped[datetime.datetime] = mapped_column(DateTime, default=datetime.datetime.utcnow)
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    user: Mapped["User"] = relationship(back_populates="posts")
+
+    comments: Mapped[list["Comment"]] = relationship(back_populates="post", cascade="all, delete-orphan")
+    likes: Mapped[list["Like"]] = relationship(back_populates="post", cascade="all, delete-orphan")
+
+
+class Comment(db.Model):
+    __tablename__ = "comment"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    content: Mapped[str] = mapped_column(String(300))
+    timestamp: Mapped[datetime.datetime] = mapped_column(DateTime, default=datetime.datetime.utcnow)
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    post_id: Mapped[int] = mapped_column(ForeignKey("post.id"))
+
+    user: Mapped["User"] = relationship(back_populates="comments")
+    post: Mapped["Post"] = relationship(back_populates="comments")
+
+
+class Like(db.Model):
+    __tablename__ = "like"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    post_id: Mapped[int] = mapped_column(ForeignKey("post.id"))
+
+    user: Mapped["User"] = relationship(back_populates="likes")
+    post: Mapped["Post"] = relationship(back_populates="likes")
